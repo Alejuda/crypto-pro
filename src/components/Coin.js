f19508ef6007cb67926e7bec32cf5ceb0fd38b0f
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-26 12:05:58 -0300

    Finish home styles

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { FaLongArrowAltUp, FaLongArrowAltDown } from 'react-icons/fa';
 
 function Coin({
   id, name, symbol, img, price, change, index,
@@ -7,25 +8,34 @@ function Coin({
   const differentStyle = index % 4 === 1 || index % 4 === 2;
   const style = differentStyle ? 'bg-blue-3' : 'bg-blue-4';
   return (
-    <NavLink className={`lato text-text-color p-5 coin ${style}`} to={`coin/${id}`}>
-      <img className="mx-auto w-16 h-auto" src={img} alt="NAME" />
-      <p className="text-center">
-        {name}
-
-      </p>
-      <p className="rounded-lg bg-slate-600 w-max px-4 mx-auto">
-        {symbol}
-      </p>
-      <p className="text-center">
-        $
-        {price >= 10 ? price.toFixed(2) : price.toFixed(4)}
-      </p>
-      <p className={change >= 0 ? 'text-green-500 text-center' : 'text-red-500 text-center'}>
-        {change < 0 ? '-' : ''}
-        %
-        {Math.abs(change)}
-      </p>
-
+    <NavLink
+      className={`lato text-text-color p-5 coin ${style}`}
+      to={`coin/${id}`}
+    >
+      <div className="flex justify-between">
+        <img className="w-16 h-auto" src={img} alt="NAME" />
+        <div className="flex flex-col justify-between items-end">
+          <p className="text-end font-bold lato text-lg">{name}</p>
+          <p className="rounded-lg bg-slate-600 px-4 text-center">{symbol}</p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <p className="text-center">
+          $
+          {price >= 10 ? price.toFixed(2) : price.toFixed(4)}
+        </p>
+        <p
+          className={
+            change >= 0
+              ? 'text-green-500 text-center flex items-center'
+              : 'text-red-500 text-center flex items-center'
+          }
+        >
+          {change < 0 ? <FaLongArrowAltDown className="text-red-500" /> : <FaLongArrowAltUp className="text-green-500" /> }
+          %
+          {Math.abs(change)}
+        </p>
+      </div>
     </NavLink>
   );
 }
